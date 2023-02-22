44855732e954b67eb9c143e6ebe107313f12c2f0
Zane Lesley <zanelesley@ou.edu>
2023-02-21 19:56:23 -0600

    part 2

--- assignment3.py
+++ assignment3.py
@@ -1,4 +1,16 @@
 list = [1,2,3]
-sum = 0 
-for x in list:
-    sum = sum + x
+
+def sum(list):
+    sum = 0 
+    for x in list:
+        sum = sum + x
+    return x
+
+def mulitplication(list):
+    product = 1
+    for x in list:
+        product = product * x
+    return product
+
+sum(list)
+mulitplication(list)
